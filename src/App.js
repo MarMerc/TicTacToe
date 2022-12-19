0ef2c8099a588ef03213079b7fdb21b4f9d9501c
MarMerc <56653754+MarMerc@users.noreply.github.com>
2022-12-19 17:11:43 -0300

    Cambios

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-import {useState} from 'react';
 import './App.css';
 import Tictactoe from './Tictactoe';
 
 function App() {
-  const [turn, setTurno]=useState('X');
 
   return (
     <div className="App">
